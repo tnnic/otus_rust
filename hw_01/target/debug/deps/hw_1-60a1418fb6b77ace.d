fc8acd87f476d08b19536952f04cabf1d2fd5293
Catsack <tnnic82@gmail.com>
2025-11-08 15:57:54 +0300

    first commit

---
+++ hw_01/target/debug/deps/hw_1-60a1418fb6b77ace.d
@@ -0,0 +1,9 @@
+C:\Users\tnnic\Desktop\rust\otus_rust\hw_01\target\debug\deps\hw_1-60a1418fb6b77ace.d: src\main.rs Cargo.toml
+
+C:\Users\tnnic\Desktop\rust\otus_rust\hw_01\target\debug\deps\libhw_1-60a1418fb6b77ace.rmeta: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
---
+++ hw_01/target/debug/deps/hw_1-d4aa430028bddc8e.d
@@ -0,0 +1,9 @@
+C:\Users\tnnic\Desktop\rust\otus_rust\hw_01\target\debug\deps\hw_1-d4aa430028bddc8e.d: src\main.rs Cargo.toml
+
+C:\Users\tnnic\Desktop\rust\otus_rust\hw_01\target\debug\deps\libhw_1-d4aa430028bddc8e.rmeta: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
