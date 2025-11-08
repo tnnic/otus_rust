d9495e18fe4e92cd2d4f653061d24a3b5c48131a
Catsack <tnnic82@gmail.com>
2025-11-08 16:06:01 +0300

    исправление дз1

---
+++ hw_01/target/debug/deps/hw_1.d
@@ -0,0 +1,5 @@
+C:\Users\tnnic\Desktop\rust\otus_rust\hw_01\target\debug\deps\hw_1.d: src\main.rs
+
+C:\Users\tnnic\Desktop\rust\otus_rust\hw_01\target\debug\deps\hw_1.exe: src\main.rs
+
+src\main.rs:
---
+++ hw_01/target/debug/hw_1.d
@@ -0,0 +1,1 @@
+C:\Users\tnnic\Desktop\rust\otus_rust\hw_01\target\debug\hw_1.exe: C:\Users\tnnic\Desktop\rust\otus_rust\hw_01\src\main.rs
